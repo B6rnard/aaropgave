a5e69eb347254a56e13e1734e7ddddd703820581
B6rnard <emilvandijk@outlook.dk>
2025-04-01 15:12:07 +0200

    base code

--- mostwanted.py
+++ mostwanted.py
@@ -1,1 +1,9 @@
-print("hej")
+import requests
+import json
+
+response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
+# parametre her   
+})
+data = json.loads(response.content)
+print(data['total'])
+print(data['items'][0]['title'])
