5eef72064f62daf36a03de3d3dd24ca95bd41b39
B6rnard <emilvandijk@outlook.dk>
2025-04-02 13:05:25 +0200

    search

--- mostwanted.py
+++ mostwanted.py
@@ -1,9 +1,11 @@
+
 import requests
 import json
 
 response = requests.get('https://api.fbi.gov/wanted/v1/list', params={
-# parametre her   
+ "warning_message" : "SHOULD BE CONSIDERED ARMED AND DANGEROUS"   
 })
 data = json.loads(response.content)
 print(data['total'])
-print(data['items'][0]['title'])
+print(data['items'][0-2]['title'])
+
